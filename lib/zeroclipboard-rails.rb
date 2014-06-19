414e6c990d8816c13152ca90a358763f3e5baccb
Hendrik Bergunde <hbergunde@gmx.de>
2014-06-19 13:22:21 +0200

    Revert "move swf to app/assets/flash as suggested in http://tinyurl.com/pfo885t"
    This reverts commit a3f16f1ea35a7d25d535a0f7941a44d766bc0863.

--- lib/zeroclipboard-rails.rb
+++ lib/zeroclipboard-rails.rb
@@ -3,7 +3,6 @@ require "zeroclipboard-rails/version"
 module Zeroclipboard
   module Rails
     class Engine < ::Rails::Engine
-      config.paths['app/assets'] << 'app/assets/flash'
     end
   end
 end
