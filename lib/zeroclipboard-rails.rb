a3f16f1ea35a7d25d535a0f7941a44d766bc0863
Hendrik Bergunde <hbergunde@gmx.de>
2014-06-19 10:46:26 +0200

    move swf to app/assets/flash as suggested in http://tinyurl.com/pfo885t

--- lib/zeroclipboard-rails.rb
+++ lib/zeroclipboard-rails.rb
@@ -3,6 +3,7 @@ require "zeroclipboard-rails/version"
 module Zeroclipboard
   module Rails
     class Engine < ::Rails::Engine
+      config.paths['app/assets'] << 'app/assets/flash'
     end
   end
 end
